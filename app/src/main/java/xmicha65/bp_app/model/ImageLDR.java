dc0840ceb764b975c79d8b6b68e28c6230d6835b
Patrik Michalák <michalak@inqool.cz>
2018-02-19 23:29:25 +0100

    refactor, image types convert methods

--- app/src/main/java/xmicha65/bp_app/model/ImageLDR.java
+++ app/src/main/java/xmicha65/bp_app/model/ImageLDR.java
@@ -6,6 +6,11 @@ import android.media.Image;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
 
@@ -16,7 +21,7 @@ import java.nio.ByteBuffer;
  */
 public class ImageLDR {
     private double exposureTime = -1;       // exposure time of image
-    private Bitmap rgbImg;                  // RGB bitmap image
+    private Bitmap bmpImg;                  // RGB bitmap image
     private int width;                      // image width
     private int height;                     // image height
     private int[] pixels;                   // raw argb android color
@@ -24,13 +29,13 @@ public class ImageLDR {
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public ImageLDR(Image image, double exposition) {
         this.exposureTime = exposition;
-        this.rgbImg = byteToBitmap(imageToByte(image));
+        this.bmpImg = byteToBitmap(imageToByte(image));
 
-        this.width = this.rgbImg.getWidth();
-        this.height = this.rgbImg.getHeight();
+        this.width = this.bmpImg.getWidth();
+        this.height = this.bmpImg.getHeight();
 
         this.pixels = new int[this.width * this.height];
-        this.rgbImg.getPixels(this.pixels, 0, this.width, 0, 0, this.width, this.height);
+        this.bmpImg.getPixels(this.pixels, 0, this.width, 0, 0, this.width, this.height);
     }
 
     /**
@@ -54,5 +59,22 @@ public class ImageLDR {
         byte[] byteArray = new byte[buffer.remaining()];
         buffer.get(byteArray);
         return byteArray;
+    }
+
+    public Bitmap matToBitmap(Mat image) {
+        Mat tmpRGBA = new Mat();
+        image.convertTo(tmpRGBA, CvType.CV_8UC4, 255);
+        Bitmap bmp = Bitmap.createBitmap(tmpRGBA.cols(), tmpRGBA.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(tmpRGBA, bmp);
+        return bmp;
+    }
+
+    public Mat bitmapToMat(Bitmap image) {
+        Bitmap bmp32 = image.copy(Bitmap.Config.ARGB_8888, true);
+        Mat matBGRA = new Mat(bmp32.getHeight(), bmp32.getWidth(), CvType.CV_8UC4);
+        Utils.bitmapToMat(bmp32, matBGRA);
+        Mat matBGR = new Mat();
+        Imgproc.cvtColor(matBGRA, matBGR, Imgproc.COLOR_BGRA2BGR);
+        return matBGR;
     }
 }
