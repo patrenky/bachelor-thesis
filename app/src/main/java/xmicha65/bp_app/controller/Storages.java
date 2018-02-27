25c9901a55050fcb8f47673d7fc5217ef3315dd8
Patrik Michalák <michalak@inqool.cz>
2018-02-28 00:15:28 +0100

    save hdr image

---
+++ app/src/main/java/xmicha65/bp_app/controller/Storages.java
@@ -0,0 +1,42 @@
+package xmicha65.bp_app.controller;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * source: Android documentation
+ */
+public class Storages {
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Get external (images) directory */
+    public static File getPublicImagesStorageDir(String subdir) {
+        File directory = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), subdir);
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                System.out.println("### mkdirs " + directory.toString() + " failed!");
+            }
+        }
+        return directory;
+    }
+}
--- app/src/main/java/xmicha65/bp_app/model/ImageHDR.java
+++ app/src/main/java/xmicha65/bp_app/model/ImageHDR.java
@@ -1,8 +1,12 @@
 package xmicha65.bp_app.model;
 
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
+import java.io.File;
 import java.io.Serializable;
+
+import xmicha65.bp_app.controller.Storages;
 
 /**
  * HDR content of merged exposures
@@ -18,8 +22,16 @@ public class ImageHDR implements Serializable {
         this.matHdrImage = matHdrImage;
     }
 
-    public void save(String filename) {
-        System.out.println("### filename: " + filename);
+    public void save(String name) {
+        File path = Storages.getPublicImagesStorageDir("/hdr");
+        File file = new File(path, String.format("%s.hdr", name));
+        String filename = file.toString();
+        System.out.println("### saving HDR: " + filename);
+
+        if (Imgcodecs.imwrite(filename, matHdrImage))
+            System.out.println("### HDR saved");
+        else
+            System.out.println("### HDR not saved!");
     }
 
     /**
