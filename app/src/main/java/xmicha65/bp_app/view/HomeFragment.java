cead99d65156f0b2339e21eafb0ae1e4b07a7b42
Patrik Michalák <michalak@inqool.cz>
2018-04-06 19:22:07 +0200

    GUI layouts

--- app/src/main/java/xmicha65/bp_app/view/HomeFragment.java
+++ app/src/main/java/xmicha65/bp_app/view/HomeFragment.java
@@ -21,14 +21,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         view.findViewById(R.id.home_capture).setOnClickListener(this);
         view.findViewById(R.id.home_load).setOnClickListener(this);
         view.findViewById(R.id.home_settings).setOnClickListener(this);
-        view.findViewById(R.id.home_init_lampicka).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene1).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene3).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene4).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene5).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene6).setOnClickListener(this);
-        view.findViewById(R.id.home_init_scene7).setOnClickListener(this);
-        view.findViewById(R.id.home_init_pohyb).setOnClickListener(this);
     }
 
     @Override
@@ -43,38 +35,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 break;
             }
             case R.id.home_settings: {
-                break;
-            }
-            case R.id.home_init_lampicka: {
-                ((Main) getActivity()).homeSelectinitLampicka();
-                break;
-            }
-            case R.id.home_init_scene1: {
-                ((Main) getActivity()).homeSelectinitScene("scena1");
-                break;
-            }
-            case R.id.home_init_scene3: {
-                ((Main) getActivity()).homeSelectinitScene("scena3");
-                break;
-            }
-            case R.id.home_init_scene4: {
-                ((Main) getActivity()).homeSelectinitScene("scena4");
-                break;
-            }
-            case R.id.home_init_scene5: {
-                ((Main) getActivity()).homeSelectinitScene("scena5");
-                break;
-            }
-            case R.id.home_init_scene6: {
-                ((Main) getActivity()).homeSelectinitScene("scena6");
-                break;
-            }
-            case R.id.home_init_scene7: {
-                ((Main) getActivity()).homeSelectinitScene("scena7");
-                break;
-            }
-            case R.id.home_init_pohyb: {
-                ((Main) getActivity()).homeSelectinitScene("scena5pohyb");
                 break;
             }
         }
